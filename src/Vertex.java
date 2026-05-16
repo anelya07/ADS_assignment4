f5449cfd41011d67a59ac7c49cecdb0ad4a9b4ae
anelya07 <253652@astanait.edu.kz>
2026-05-16 11:37:48 +0500

    vertex

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,2 +1,17 @@
 public class Vertex {
+        private int id;
+
+        public Vertex(int id) {
+            this.id = id;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        @Override
+        public String toString() {
+            return "Vertex(" + id + ")";
+        }
 }
+
