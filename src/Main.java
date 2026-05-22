42ac3af2d91d2a190d4c6db1fdbb85e10a8adfda
anelya07 <253652@astanait.edu.kz>
2026-05-22 16:32:36 +0500

    main(Dijkstra)

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,19 @@ public class Main {
         Experiment experiment = new Experiment();
         experiment.runMultipleTests();
         experiment.printResults();
+
+        System.out.println("\n==== Dijkstra Algorithm =====");
+        Graph g = new Graph(6);
+        g.addWeightedEdge(0, 1, 4);
+        g.addWeightedEdge(0, 2, 2);
+        g.addWeightedEdge(1, 3, 5);
+        g.addWeightedEdge(2, 3, 1);
+        g.addWeightedEdge(2, 4, 3);
+        g.addWeightedEdge(3, 5, 2);
+        g.addWeightedEdge(4, 5, 4);
+
+        g.printGraph();
+        System.out.println();
+        g.dijkstra(0);
     }
 }
